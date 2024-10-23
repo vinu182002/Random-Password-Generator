f14a2d5fe4a0ac300f63e825babd0950c9920b6a
vinu182002 <126711197+vinu182002@users.noreply.github.com>
2024-10-23 13:29:22 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]|:;<>,.?/~`";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById("result").textContent = "Generated Password: " + password;
+}
